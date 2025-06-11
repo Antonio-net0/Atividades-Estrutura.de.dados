71c9a50f82e7abfd93d1fc3257e56b97a5037851
Antônio F. O. Neto <antonio.fer.oli.neto@gmail.com>
2025-06-11 14:40:55 -0300

    Mudei o tipo de variável nos metodos de ordenação
    Alterei os dois métodos de ordenação (nome e idade),. A variável auxiliar ficou "Idadenome aux" para receber todos os valores e organizar em ordem crescente ou decrescente  com os dados dos usuários em ordem.
    
    Também criei um novo vetor para que eu consiga organizar por cadegoria idade e categoria nome.

--- Método da bolha/Idadenome.java
+++ Método da bolha/Idadenome.java
@@ -1,4 +1,4 @@
-package Nome.e;
+package Bumble_Sorte;
 
 import java.util.Scanner;
 
@@ -7,10 +7,11 @@ public class Idadenome {
     int idade;
     String nome;
 
-    public Idadenome(int idade, String nome) {
+    public Idadenome(String nome, int idade) {
         this.idade = idade;
         this.nome = nome;
     }
+
     @Override
     public String toString() {
         return "Idadenome{" +
@@ -18,49 +19,57 @@ public class Idadenome {
                 ", nome='" + nome + '\'' +
                 '}';
     }
+
     public static void ordenarPorIdade(Idadenome[] v){
-            int aux;
+            Idadenome aux;
             for(int cont = 0; cont < v.length-1; cont++){
                 for(int cont2= cont +1; cont2 < v.length; cont2++){
                     if(v[cont].idade>v[cont2].idade){
-                        aux = v[cont].idade;
-                        v[cont].idade=v[cont2].idade;
-                        v[cont2].idade=aux;
+                        aux = v[cont];
+                        v[cont]=v[cont2];
+                        v[cont2]=aux;
                     }
                 }
             }
         }
         public static void ordenarPorNome(Idadenome[] t){
-            String aux;
+            Idadenome aux;
             for(int cont = 0; cont < t.length-1; cont++) {
                 for (int cont2 = cont + 1; cont2 < t.length; cont2++) {
                     if(t[cont].nome.compareTo(t[cont2].nome)>0){
                         //>0 primeiro maior que segundo
-                        // <0 primeiro menor que segundo
-                        aux= t[cont].nome;
-                        t[cont].nome= t[cont2].nome;
-                        t[cont2].nome=aux;
+                        //<0 primeiro menor que segundo
+                        aux= t[cont];
+                        t[cont]= t[cont2];
+                        t[cont2]=aux;
                     }
                 }
             }
         }
         public static void main (String[]args){
+            Scanner ler = new Scanner(System.in);
             Idadenome[] selecionar = new Idadenome[5];
+            Idadenome[] selecionarN=new Idadenome[5];
             for (int i = 0; i < selecionar.length; i++){
-                Scanner ler = new Scanner(System.in);
                     System.out.println("Insira nome: ");
                     String nome = ler.nextLine();
                     System.out.println("Insira idade: ");
                     int idade = ler.nextInt();
-                    selecionar[i]=new Idadenome(idade, nome);
+                    ler.nextLine();
+                    selecionar[i]=new Idadenome(nome, idade);
+                    selecionarN[i]=new Idadenome(nome,idade);
             }
-            ordenarPorNome(selecionar);
+            ordenarPorNome(selecionarN);
             ordenarPorIdade(selecionar);
             for (int i = 0; i < selecionar.length; i++) {
-                System.out.println("Ordenção de idades e nomes em ordem crescente: ");
-                System.out.println("vet[" + i + "] = " + selecionar[i].idade + "  " + selecionar[i].nome);
+                System.out.println("Ordenção de idades ");
+                System.out.println("vet[" + i + "] = " + selecionar[i]);
+                System.out.println("Ordenação de nomes: " );
+                System.out.println("vet[" + i + "] = " + selecionarN[i]);
             }
+            ler.close();
         }
+
     }
 
 
